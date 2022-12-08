0424496a3bb3087f328d705de961f00bd395ea11
Mickman321 <97745994+Mickman321@users.noreply.github.com>
2022-12-08 09:35:13 +0100

    First commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float walkSpeed;
+    public float sprintSpeed;
+
+    Vector3 velocity;
+
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+
+    public float groundDistance = 0.4f;
+
+    public LayerMask groundMask;
+
+    bool isGrounded;
+
+
+
+    // det dem här variablerna gör är att, kolla gravitation och öka velocity, hur mycket distance du är från marken, om spelaren är på marken, att referera till character controller i unity, hur mycket fart spelaren har.
+    //Animations skapad av Niljas och scriptade, Används dock inte eftersom jag använder en annan script för animations.
+    private bool isMovingForward = false;
+    private bool isMovingBackwards = false;
+
+    Animator Am;
+    Rigidbody rb;
+
+    public MovementState state;
+    public enum MovementState
+    {
+        walking,
+        sprinting,
+        air
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        //Debug.DrawRay(groundCheck.position, -Vector3.up * groundDistance);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+    }// Det den här koden gör är att få spelaren att gå fram, back, vänster, höger och kolla gravitation
+
+    private void StateHandler()
+    {
+        // Mode - Sprinting
+       /* if(isGrounded) && Input.GetKey(sprintKey))
+        {
+        
+        }*/
+    }
+
+}
+
+
---
+++ Assets/Scripts/Random array for deadline.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Randomarrayfordeadline : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        // this is an array, for loop and while loop.
+        
+        for (int i = 0; i < 10; i++)
+        {
+            int[] myArray = new int[] { 1, 2, 3, 4, 5 };
+        }
+
+        int x = 0;
+        while (x < 10)
+        {
+            int[] myArray = new int[] { 1, 2, 3, 4, 5 };
+        }
+    }
+}
